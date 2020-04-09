af97eb7cb8322dee533f07cdce9b42777fe056ac
Tiago Cunha <1700597@sal.ipg.pt>
2020-04-09 16:39:06 +0100

    Resultado final sorteio de 0 a 10

--- app/src/main/java/pt/ipg/sorteio/MainActivity.java
+++ app/src/main/java/pt/ipg/sorteio/MainActivity.java
@@ -3,6 +3,10 @@ package pt.ipg.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarBotao(View v){
+
+        int x = new Random().nextInt(11);
+
+        TextView texto = findViewById((R.id.textoResultado));
+        texto.setText("O numero sorteado foi: " + x);
+
+
+
     }
 }
